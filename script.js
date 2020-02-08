16a872c1688a38cb7eb0a306a122413d05080af6
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-02-08 15:18:19 -0300

    Add LocalStorage

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
 
+$(document).ready(function() {
     var table=$('#movieTable').DataTable({
         "paging":false,
         "searching":false,
@@ -15,27 +15,49 @@ $(document).ready(function() {
             return JSON.parse( localStorage.getItem('DataTables') );
         }
     });
+    if(localStorage.getItem('MoviesList')!=null) {
+        var data = JSON.parse(localStorage.getItem('MoviesList'));
+    }else{
+        var data={};
+    }
     
+    
+    for(var key in data){
+        table.row.add([key,data[key],"<button class='btn btn-block btn-danger delete'>Delete</button>"]).draw();
+    }
     $("#add").on("click",function(e){
         
         e.preventDefault();
+        //var movieTitle=capitalize_Words($("#movieTitle").val().toLowerCase());
+        var movieTitle=$("#movieTitle").val();
+
+
         var inputVal =parseInt($('#rate').val());
         console.log($('#rate').val());
         if(inputVal<0 || inputVal>10 || Number.isNaN(inputVal)) alert("Rating value must be between 0 and 10")
         else {
-        table.row.add([$("#movieTitle").val(),$('#rate').val(),"<button class='btn btn-block btn-danger delete'>Delete</button>"] 
-            ).draw();
-            $('#movieTitle').val("");
-            $('#rate').val("");
+            if(data[movieTitle]!=undefined) alert(movieTitle+" alredy exists");
+            else{
+                data[movieTitle]=inputVal;
+                localStorage.setItem( 'MoviesList', JSON.stringify(data) );
+                table.row.add([movieTitle,$('#rate').val(),"<button class='btn btn-block btn-danger delete'>Delete</button>"] 
+                    ).draw();
+                    $('#movieTitle').val("");
+                    $('#rate').val("");
 
-        }
+                }
+                $("tr > td").addClass("align-middle");
+            }
         
     });
     $("#movieTable").on('click','.delete',function(){
+        delete data[$($(this).parent().parent().children("td")[0]).html()];
+        localStorage.setItem( 'MoviesList', JSON.stringify(data) );
         table
             .row($(this).parents())
             .remove()
             .draw();
+
     });
     
 } );
